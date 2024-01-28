cf2e1d7e17e160691d6f89708b04bcb8625c6327
Pasan Madhuranga <121794032+MADHURANGA-SKP@users.noreply.github.com>
2024-01-28 18:44:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,6 @@ import Navbar from "./components/Navbar";
 import { getUser } from "./redux/userSlice";
 import UpdateUser from "./UpdateUser";
 
-/*
-"Users", "CreateUser", "UpdateUser" are components defined in other files, likely responsible for rendering different views or pages in your application.
-
-"bootstrap/dist/css/bootstrap.min.css"  imports the Bootstrap CSS stylesheet, providing styling for whole application.
-
-"BrowserRouter", "Routes", "Route" are components from the "react-router-dom" library, used for handling "client-side" routing in React applications.
-
-"useEffect" is a hook in React that allows you to perform side effects in your functional components. In this case, it's used to make an asynchronous request to fetch data.
-
-"axios" It's a library for making HTTP requests. Here, it's used to fetch data from a server.
-
-"useDispatch" It's a hook provided by "react-redux" for accessing the dispatch function. It's used to dispatch actions to the Redux store.
-
-"Navbar" is the component that uses for rendering a navigation bar.
-
-"getUser" is an action creator from the Redux store, possibly defined in "./redux/userSlice". It's used to dispatch an action with fetched user data.
-*/
-
 function App() {
   const dispatch = useDispatch();
 
@@ -62,9 +44,7 @@ function App() {
 export default App;
 /* 
 <BrowserRouter> provides the context for the routing in your application.
-
 <Navbar /> Renders the navigation bar component.
-
 <Routes> and <Route> components that define the routes in your application. The element prop specifies the component to render when the route is matched.
 
 defined routes:-----------------------------------------------------------------------
